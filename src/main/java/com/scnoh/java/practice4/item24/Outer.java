998854f942d02c31b629ef3f3f550f286c2c12b9
s_chuli <stc9606@naver.com>
2023-04-03 21:13:26 +0900

    아이템26. 로 타입은 사용하지 말라
    - 타입의 안정성을 보장하지 않음.
    - 코드 가독성을 떨어뜨림.
    - 형변환 코드를 추가로 작성해야 함.

---
+++ src/main/java/com/scnoh/java/practice4/item24/Outer.java
@@ -0,0 +1,13 @@
+package com.scnoh.java.practice4.item24;
+
+public class Outer {
+    public Outer() {
+
+    }
+
+    public static class Inner {
+        public Inner() {
+
+        }
+    }
+}
---
+++ src/main/java/com/scnoh/java/practice4/item24/Test.java
@@ -0,0 +1,6 @@
+package com.scnoh.java.practice4.item24;
+
+public class Test {
+    Outer.Inner instance = new Outer.Inner();
+
+}
