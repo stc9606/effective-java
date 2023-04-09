a57067fc1d89849b72bfbc50c7486d3b01aea258
s_chuli <stc9606@naver.com>
2023-04-09 20:49:44 +0900

    아이템30. 제네릭 메서드를 사용해 강력하고 유연한 타입 안전성을 얻으라
    - 제네릭 싱글턴 팩터리 메서드를 사용하면, 싱글턴 팩터리 메서드로 만들어 간결하게 작성할 수 있다. 타입 별로 클래스를 만들지 않아도 되며, 타입마다 하나의 싱글턴 객체를 생성해준다.

--- src/main/java/com/scnoh/java/practice5/item30/SetMerge.java
+++ src/main/java/com/scnoh/java/practice5/item30/SetMerge.java
@@ -1,2 +1,20 @@
-package com.scnoh.java.practice5.item30;public class SetMerge {
+package com.scnoh.java.practice5.item30;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMerge {
+    public static void main(String[] args) {
+        Set<String> guys = Set.of("톰", "딕", "해리");
+        Set<String> stooges = Set.of("래리", "모에", "컬리");
+        Set<String> aflCio = union(guys, stooges);
+        System.out.println(aflCio);
+
+    }
+
+    public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
+        Set<E> result = new HashSet<>(s1);
+        result.addAll(s2);
+        return result;
+    }
 }
