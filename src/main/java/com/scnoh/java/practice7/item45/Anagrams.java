96fb1b3b85fef857d90e7b43dd6c1a9fa2f3b3be
scnoh <stc9606@naver.com>
2023-12-01 20:33:55 +0900

    item45. 스트림은 주의해서 사용하라. (적절한 stream 사용 예시)

--- src/main/java/com/scnoh/java/practice7/item45/Anagrams.java
+++ src/main/java/com/scnoh/java/practice7/item45/Anagrams.java
@@ -2,7 +2,14 @@ package com.scnoh.java.practice7.item45;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Anagrams {
 
@@ -11,11 +18,34 @@ public class Anagrams {
         int minGroupSize = Integer.parseInt(args[1]);
 
         Map<String, Set<String>> groups = new HashMap<>();
+
+        // 스트림 사용 전
         try (Scanner scanner = new Scanner(dictionary)) {
             while (scanner.hasNext()) {
                 String word = scanner.next();
                 groups.computeIfAbsent(alphabetize(word), (unused) -> new TreeSet<>()).add(word);
             }
+        }
+
+        // 스트림 과하게 사용
+        Path dictionaryPath = Paths.get(args[0]);
+        try (Stream<String> words = Files.lines(dictionaryPath)) {
+            words.collect(
+                    groupingBy(word -> word.chars().sorted()
+                            .collect(StringBuilder::new, (sb, s) -> sb.append((char) s),
+                                    StringBuilder::append).toString()))
+                    .values().stream()
+                    .filter(group -> group.size() >= minGroupSize)
+                    .map(group -> group.size() + ": " + group)
+                    .forEach(System.out::println);
+        }
+
+        // 적절한 스트림 사용
+        try (Stream<String> words = Files.lines(dictionaryPath)) {
+            words.collect(groupingBy(Anagrams::alphabetize))
+                    .values().stream()
+                    .filter(group -> group.size() >= minGroupSize)
+                    .forEach(g -> System.out.println(g.size() + ": " + g));
         }
 
         for (Set<String> group : groups.values()) {
