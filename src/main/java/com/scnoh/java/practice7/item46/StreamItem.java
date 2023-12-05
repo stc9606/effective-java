9ad39538ae0310f15049d0115441392b63ee8c04
scnoh <stc9606@naver.com>
2023-12-05 22:32:47 +0900

    item46. 스트림에는 부작용 없는 함수를 사용하라.
    - 스트림 패러다임을 이해하지 못한 코드와 제대로 활용한 코드 비교

--- src/main/java/com/scnoh/java/practice7/item46/StreamItem.java
+++ src/main/java/com/scnoh/java/practice7/item46/StreamItem.java
@@ -1,9 +1,9 @@
 package com.scnoh.java.practice7.item46;
 
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
@@ -11,13 +11,30 @@ import static java.util.stream.Collectors.toList;
 public class StreamItem {
 
     public static void main(String[] args) {
-        Map<String, String> freq = new HashMap<>();
+        List<String> words = new ArrayList<>();
+        words.add("stop");
+        words.add("spot");
+        words.add("trim");
+        words.add("meet");
+        words.add("ball");
+        words.add("free");
+        words.add("trim");
+        words.add("meet");
+
+        Map<String, Long> freq = new HashMap<>();
+        // 스트림 패러다임을 이해하지 못한 채 API만 사용
+        words.forEach(word -> freq.merge(word.toLowerCase(), 1L, Long::sum));
+
+        // 스트림을 제대로 활용해 빈도표를 초기화한다.
+//        freq = words.stream().collect(groupingBy(String::toLowerCase, counting()));
+
+        System.out.println("words = " + words);
+        System.out.println("freq = " + freq);
+
         // 가장 흔한 단어 10개를 뽑아내는 파이프라인
-        freq.keySet().stream()
+        List<String> collect = freq.keySet().stream()
                 .sorted(comparing(freq::get).reversed())
                 .limit(10)
                 .collect(toList());
-
-
     }
 }
